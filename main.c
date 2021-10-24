42fcd963b87a61f78490e3d5b91da211c49953be
Andrew Nitchman <nitchma4@msu.edu>
2021-10-24 08:59:45 -0400

    test message
    ideally this should explain the change made

--- main.c
+++ main.c
@@ -1,20 +1,11 @@
 /* p11_1.c Toggling P2.7 at 60 Hz using Timer_A0.4 PWM
  *
- * This program uses Timer_A0.4 to generate PWM output at 60 Hz
- * and 33.3% duty cycle.
- * Subsystem Master Clock (SMCLK) running at 3 MHz is used.
- * Timer_A0 is configured to count up from 0 to 50,000-1, which
- * is loaded in TIMER_A0->CCR[0].
- * The timer counter roll over interval is:
- * 3,000,000 / 50,000 = 60 Hz.
- * TIMER_A0->CCR[4] is loaded with 50000/3 and CCR4 is configured
- * as reset/set mode. The output is set to 1 at the beginning
- * of the counting cycle when the counter is 0. When the counter
- * counts up to 50000/3, the output is reset to 0 and stays 0 until
- * the next counting cycle. So the output stays at 1 for 1/3=33.3% of the time.
+ *testing
+ *testing
  *
- * Tested with Keil 5.20 and MSP432 Device Family Pack V2.2.0
- * on XMS432P401R Rev C.
+ *
+ *
+ *
  */
 
 #include "msp.h"
